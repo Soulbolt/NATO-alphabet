0da1658bc8e72f5d0f1589520f83f7febe88f1f7
Soulbolt <soulbolt@gmail.com>
2023-12-22 11:30:08 -0700

    implemented function with try exception to request user for correct input and prevent program from crashing on keyError.

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import pandas
 
-student_dict = {
-    "student": ["Angela", "James", "Lily"],
-    "score": [56, 76, 98]
-}
+# student_dict = {
+#     "student": ["Angela", "James", "Lily"],
+#     "score": [56, 76, 98]
+# }
 
-# Looping through dictionaries:
-for (key, value) in student_dict.items():
-    # Access key and value
-    pass
+# # Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     # Access key and value
+#     pass
 
-import pandas
+# import pandas
 
-student_data_frame = pandas.DataFrame(student_dict)
+# student_data_frame = pandas.DataFrame(student_dict)
 
-# Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    # Access index and row
-    # Access row.student or row.score
-    pass
+# # Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     # Access index and row
+#     # Access row.student or row.score
+#     pass
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
@@ -30,6 +30,15 @@ df = pandas.read_csv("nato_phonetic_alphabet.csv")
 new_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 # print(new_dict)
 # 2. Create a list of the phonetic code words from a word that the user inputs.
-user_word = input("Please enter name to convert: \n").upper()
-alpha_conversion = [new_dict[letter] for letter in user_word]
-print(alpha_conversion)
+# create try exception to loop until user privides correct values
+def generate_phonetic():
+    user_word = input("Please enter name to convert: \n").upper()
+    try:
+        alpha_conversion = [new_dict[letter] for letter in user_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(alpha_conversion)
+
+generate_phonetic()
